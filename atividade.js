0b244cc2a5d25df293584f813730a7db94c68d13
Carlos Machado <168152992+DevCarlosMachado@users.noreply.github.com>
2024-09-16 13:02:09 +0000

    atividade.js

--- atividade.js
+++ atividade.js
@@ -13,3 +13,23 @@ var cidade = "Olinda";
 console.log("Olá meu nome é " + nome + " e moro em " + cidade + ".");
 
 // atividade 3 
+console.log("\nAtividade 3:")
+var num1 = 45;
+var num2 = 5;
+var soma, subtracao, multiplicacao, divisao;
+
+//soma
+soma = num1 + num2;
+console.log("O valor da soma entre " + num1 + " e " + num2 + " é: " + soma + ".");
+//subtração
+subtracao = num1 - num2;
+console.log("O valor da subtração entre " + num1 + " e " + num2 + " é: " + subtracao + ".");
+//multiplicação
+multiplicacao = num1 * num2;
+console.log("O valor da multiplicação entre " + num1 + " e " + num2 + " é: " + multiplicacao + ".");
+//divisão
+divisao = num1 / num2;
+console.log("O valor da divisão entre " + num1 + " e " + num2 + " é: " + divisao + ".");
+
+//atividade 4
+console.log("\nAtividade 4:"); 
