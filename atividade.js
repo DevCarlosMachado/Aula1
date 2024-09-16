31a0445f60417279283312ff15e13f0e7e05368e
Carlos Machado <168152992+DevCarlosMachado@users.noreply.github.com>
2024-09-16 12:36:52 +0000

    Atividade.js

---
+++ atividade.js
@@ -0,0 +1,15 @@
+// atividade 1
+console.log("Atividade 1:");
+var nome = "Carlos";
+var idade = 20;
+
+console.log("Olá meu nome é " + nome + " e tenho " + idade + " anos.")
+
+// atividade 2
+console.log("\nAtividade 2:")
+var nome = "Alice";
+var cidade = "Olinda";
+
+console.log("Olá meu nome é " + nome + " e moro em " + cidade + ".");
+
+// atividade 3 
