1afa8aeb7fcbb07b6d4a92ae1a1e550e9d3cb2f7
Carlos Machado <168152992+DevCarlosMachado@users.noreply.github.com>
2024-09-19 14:38:05 +0000

    atividade.js

--- atividade.js
+++ atividade.js
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')();
+
 // atividade 1
 console.log("Atividade 1:");
 var nome = "Carlos";
@@ -33,3 +35,27 @@ console.log("O valor da divisão entre " + num1 + " e " + num2 + " é: " + divis
 
 //atividade 4
 console.log("\nAtividade 4:"); 
+var base = 10;
+var altura = 5;
+var areaTriangulo = base*altura/2;
+console.log(`A área do triângulo é  ${areaTriangulo}`);
+
+//atividade 5
+console.log("\nAtividade 5:");
+var nota1 = 8;
+var nota2 = 5;
+var nota3 = 6;
+var media = ((nota1 + nota2 + nota3)/3).toFixed(2);
+console.log(`O resultado da média é ${media}`);
+var resultado = media > 7 ? 'aprovado' : 'reprovado';
+console.log(`Você foi ${resultado}`);
+
+//atividade 6
+console.log("\nAtividade 6:");
+var valorProduto = 1200;
+var desconta = 0.1;
+var valorDesconto = valorProduto - (0.1*1200);
+console.log(`O valor do produto com desconto é de R$ ${valorDesconto}`);
+
+//atividade 7
+console.log("\nAtividade 7:");
